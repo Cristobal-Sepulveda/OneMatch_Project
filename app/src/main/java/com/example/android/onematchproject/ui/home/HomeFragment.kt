93f88ccd9e018545a006fb681fd07fb47c3534e9
Cristobal-Sepulveda <sepulveda.cristobal.ignacio@gmail.com>
2021-05-13 14:37:37 -0400

    profile layout almost done

--- app/src/main/java/com/example/android/onematchproject/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/android/onematchproject/ui/home/HomeFragment.kt
@@ -4,27 +4,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.android.onematchproject.R
-import com.example.android.onematchproject.utils.CommonVariables
-import com.example.android.onematchproject.utils.CommonVariables.firebaseAuth
-import com.google.firebase.firestore.FirebaseFirestore
+import com.example.android.onematchproject.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
+    private lateinit var viewModel: HomeViewModel
+    private lateinit var binding: FragmentHomeBinding
 
-    override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
-        homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
 
-        return root
+        viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+
+        return binding.root
     }
 }
--- app/src/main/java/com/example/android/onematchproject/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/android/onematchproject/ui/profile/ProfileFragment.kt
@@ -4,21 +4,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.android.onematchproject.R
+import com.example.android.onematchproject.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
 
     private lateinit var viewModel: ProfileViewModel
-
+    private lateinit var binding: FragmentProfileBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?): View? {
+                              savedInstanceState: Bundle?): View {
 
         viewModel = ViewModelProvider(this).get(ProfileViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_profile, container, false)
 
-        return root
+        return binding.root
     }
 }
